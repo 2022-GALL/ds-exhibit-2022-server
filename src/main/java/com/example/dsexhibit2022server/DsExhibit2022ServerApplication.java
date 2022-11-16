4849a23013cc370b78112a5f28bd6253f4e21780
jeeheaG <jeehea99@naver.com>
2022-11-17 01:57:20 +0900

    Init : First commit. Make Project

---
+++ src/main/java/com/example/dsexhibit2022server/DsExhibit2022ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsexhibit2022server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsExhibit2022ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsExhibit2022ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dsexhibit2022server/DsExhibit2022ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsexhibit2022server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsExhibit2022ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
